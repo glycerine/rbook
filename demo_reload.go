bf8ede2fe4be8a41dc27254acde68442eec7e61c
Jason E. Aten <j.e.aten@gmail.com>
2022-11-14 00:27:28 -0600

    minibook start

---
+++ demo_reload.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func demoReloadServer() {
+	log.Println("Starting reload server.")
+
+	startReloadServer()
+
+	log.Println("Reload server started.")
+	log.Println("Press Enter to reload the browser!")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		reader.ReadString('\n')
+
+		log.Println("Reloading browser.")
+		sendReload()
+	}
+}
--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"log"
-	"os"
+//	"bufio"
+//	"log"
+//	"os"
 )
 
 func main() {
-	log.Println("Starting reload server.")
-
-	startReloadServer()
-
-	log.Println("Reload server started.")
-	log.Println("Press Enter to reload the browser!")
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		reader.ReadString('\n')
-
-		log.Println("Reloading browser.")
-		sendReload()
-	}
+	demoReloadServer()
 }
