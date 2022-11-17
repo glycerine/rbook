17b09e785e76bb64bdfa5c0afcbb2040cac2ec02
Jason E. Aten <j.e.aten@gmail.com>
2022-11-16 19:52:36 -0600

    quiet

--- rbookcfg.go
+++ rbookcfg.go
@@ -14,7 +14,7 @@ type RbookConfig struct {
 func (c *RbookConfig) DefineFlags(fs *flag.FlagSet) {
 	fs.StringVar(&c.Host, "host", "", "host/ip to server on (optional)")
 	fs.IntVar(&c.Port, "port", 8080, "port to serve index.html for images/R updates on.")
-	fs.StringVar(&c.RbookFilePath, "path", "", "path to the .rbook file to read and append to. this is also the default command line argument, so -path can be omitted in front of the path")
+	fs.StringVar(&c.RbookFilePath, "path", "", "path to the .rbook file to read and append to. this is also the default command line argument, so -path can be omitted in front of the path (default is my.rbook in the current dir)")
 }
 
 // call c.ValidateConfig() after myflags.Parse()
@@ -24,10 +24,10 @@ func (c *RbookConfig) ValidateConfig(fs *flag.FlagSet) error {
 		args := fs.Args()
 		if len(args) == 1 {
 			c.RbookFilePath = args[0]
-			vv("set c.RbookFilePath to '%v'", c.RbookFilePath)
+			//vv("set c.RbookFilePath to '%v'", c.RbookFilePath)
 		} else {
 			c.RbookFilePath = "my.rbook"
-			vv("defaulting c.RbookFilePath to '%v'", c.RbookFilePath)
+			//vv("defaulting c.RbookFilePath to '%v'", c.RbookFilePath)
 		}
 	}
 	return nil
