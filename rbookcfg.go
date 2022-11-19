0796258741e6a5398cfc7352ecec74bfc8985927
Jason E. Aten <j.e.aten@gmail.com>
2022-11-18 23:51:42 -0600

    rbook -h gives help

--- rbookcfg.go
+++ rbookcfg.go
@@ -31,7 +31,7 @@ func (c *RbookConfig) DefineFlags(fs *flag.FlagSet) {
 	fs.IntVar(&c.Port, "port", 0, "port to serve index.html for images/R updates on (optional; if -port is taken or 0, defaults to the first free port at or above 8888)")
 	fs.StringVar(&c.RbookFilePath, "path", "", "path to the .rbook file to read and append to. this is also the default command line argument, so -path can be omitted in front of the path (default is my.rbook in the current dir)")
 	fs.StringVar(&c.Rhome, "rhome", "/usr/lib/R", "value of R_HOME to start R with. This directory should have contents: bin  COPYING  etc  lib  library  modules  site-library  SVN-REVISION")
-	fs.BoolVar(&c.Help, "help", false, "show this help")
+	fs.BoolVar(&c.Help, "help", false, "show this help given rbook -h")
 }
 
 // call c.ValidateConfig() after myflags.Parse()
