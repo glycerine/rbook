8f2f0f4551577d31c733b09ec578a738fcac519d
Jason E. Aten <j.e.aten@gmail.com>
2022-11-19 16:54:49 -0600

    wallpaper from abs path

--- xvfb.go
+++ xvfb.go
@@ -44,8 +44,12 @@ func (c *RbookConfig) StartXvfbAndFriends(display string) {
 
 	c.xvfb = startInBackground("/usr/bin/Xvfb", strings.Split(display+" -screen 0 3000x2000x16", " ")...)
 	c.icewm = startInBackground("/usr/bin/icewm")
+
 	// give it a nice wallpaper
-	go startInBackground("/usr/bin/feh", "--bg-scale", "misc/pexels-ian-turnell-709552.jpg").Wait()
+	wall := "/home/jaten/go/src/github.com/glycerine/rbook/misc/pexels-ian-turnell-709552.jpg"
+	if FileExists(wall) {
+		go startInBackground("/usr/bin/feh", "--bg-scale", wall).Wait()
+	}
 	c.x11vnc = startInBackground("/usr/bin/x11vnc", "-display", display, "-forever", "-nopw", "-quiet", "-xkb")
 }
 
